838ce01ce8cc68c3a454f0f5bae32375fa327680
Nicholas Bering <bering.nicholas@gmail.com>
2018-02-25 20:09:27 -0500

    Add support for ansible_group resource.
    Closes #1

--- terraform.py
+++ terraform.py
@@ -35,17 +35,33 @@ def _extract_list(attrs, key):
 
     return out
 
+def _init_group(children=None, hosts=None, vars=None):
+    return {
+        "hosts": [] if hosts is None else hosts,
+        "vars": {} if vars is None else vars,
+        "children": [] if children is None else children
+    }
+
 def _add_host(inventory, hostname, groups, host_vars):
     inventory["_meta"]["hostvars"][hostname] = host_vars
     for group in groups:
         if group not in inventory.keys():
-            inventory[group] = [hostname]
+            inventory[group] = _init_group(hosts=[hostname])
         elif hostname not in inventory[group]:
-            inventory[group].append(hostname)
+            inventory[group]["hosts"].append(hostname)
+
+def _add_group(inventory, group_name, children, group_vars):
+    if group_name not in inventory.keys():
+        inventory[group_name] = _init_group(children=children, vars=group_vars)
+    else:
+        # Start out with support for only one "group" with a given name
+        # If there's a second group by the name, last in wins
+        inventory[group_name]["children"] = children
+        inventory[group_name]["vars"] = group_vars
 
 def _init_inventory():
     return {
-        "all": [],
+        "all": _init_group(),
         "_meta": {
             "hostvars": {}
         }
@@ -61,6 +77,13 @@ def _handle_host(attrs, inventory):
 
     _add_host(inventory, hostname, groups, host_vars)
 
+def _handle_group(attrs, inventory):
+    group_vars = _extract_dict(attrs, "vars")
+    children = _extract_list(attrs, "children")
+    group_name = attrs["inventory_group_name"]
+
+    _add_group(inventory, group_name, children, group_vars)
+
 def _walk_state(tfstate, inventory):
     for module in tfstate["modules"]:
         for resource in module["resources"].values():
@@ -71,6 +94,8 @@ def _walk_state(tfstate, inventory):
 
             if resource["type"] == "ansible_host":
                 _handle_host(attrs, inventory)
+            if resource["type"] == "ansible_group":
+                _handle_group(attrs, inventory)
 
     return inventory
 
