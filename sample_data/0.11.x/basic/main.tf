262b3993449501e3792876b396004d59c14254e9
Nicholas Bering <bering.nicholas@gmail.com>
2019-05-05 08:37:32 -0400

    Automatic formatting correction.

--- sample_data/0.11.x/basic/main.tf
+++ sample_data/0.11.x/basic/main.tf
@@ -1,36 +1,36 @@
 resource "ansible_host" "www" {
-    inventory_hostname = "www.example.com"
-    groups             = ["example", "web"]
-    vars {
-        fooo = "aaa"
-        bar  = "bbb"
-    }
+  inventory_hostname = "www.example.com"
+  groups             = ["example", "web"]
+  vars {
+    fooo = "aaa"
+    bar  = "bbb"
+  }
 }
 
 resource "ansible_host" "db" {
-    inventory_hostname = "db.example.com"
-    groups             = ["example", "db"]
-    vars {
-        fooo = "ccc"
-        bar  = "ddd"
-    }
+  inventory_hostname = "db.example.com"
+  groups             = ["example", "db"]
+  vars {
+    fooo = "ccc"
+    bar  = "ddd"
+  }
 }
 
 resource "ansible_group" "web" {
-    inventory_group_name = "web"
-    children = ["foo", "bar", "baz"]
-    vars {
-        foo = "bar"
-        bar = 2
-    }
+  inventory_group_name = "web"
+  children             = ["foo", "bar", "baz"]
+  vars {
+    foo = "bar"
+    bar = 2
+  }
 }
 
 # A host with no optional properties.
 resource "ansible_host" "base" {
-    inventory_hostname = "base.example.com"
+  inventory_hostname = "base.example.com"
 }
 
 # A group with no optional properties.
 resource "ansible_group" "base" {
-    inventory_group_name = "base"
+  inventory_group_name = "base"
 }
