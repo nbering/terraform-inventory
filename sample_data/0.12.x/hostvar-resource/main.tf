b8f8ced17110aaf403e6675cbc68ad3704b5d85f
Nicholas Bering <bering.nicholas@gmail.com>
2019-05-20 04:21:42 -0400

    Add test case and expected output for #9

---
+++ sample_data/0.12.x/hostvar-resource/main.tf
@@ -0,0 +1,23 @@
+resource "ansible_host" "www" {
+  inventory_hostname = "www.example.com"
+  groups             = ["example", "web"]
+  vars = {
+    foo = "aaa"
+    bar = "bbb"
+  }
+}
+
+resource "ansible_host" "db" {
+  inventory_hostname = "db.example.com"
+  groups             = ["example", "db"]
+  vars = {
+    foo = "ccc"
+    bar = "ddd"
+  }
+}
+
+resource "ansible_host_var" "extra" {
+  inventory_hostname = "www.example.com"
+  key                = "db_host"
+  value              = "${ansible_host.db.inventory_hostname}"
+}
