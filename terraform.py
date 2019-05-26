fd44643bb83d8932e3688d290cc9fb2eb4326b9e
Nicholas Bering <bering.nicholas@gmail.com>
2019-05-26 03:04:09 -0400

    Add docstrings.

--- terraform.py
+++ terraform.py
@@ -48,6 +48,10 @@ TERRAFORM_WS_NAME = os.environ.get('ANSIBLE_TF_WS_NAME', 'default')
 
 
 class TerraformState(object):
+    '''
+    TerraformState wraps the state content to provide some helpers for iterating
+    over resources.
+    '''
     def __init__(self, state_json):
         self.state_json = state_json
 
@@ -59,6 +63,7 @@ class TerraformState(object):
             self.flat_attrs = False
 
     def resources(self):
+        '''Generator method to iterate over resources in the state file.'''
         if self.flat_attrs:
             modules = self.state_json["modules"]
             for module in modules:
@@ -72,20 +77,32 @@ class TerraformState(object):
 
 
 class TerraformResource(object):
+    '''
+    TerraformResource wraps individual resource content and provide some helper
+    methods for reading older-style dictionary and list values from attributes
+    defined as a single-level map.
+    '''
     def __init__(self, source_json, flat_attrs=False, resource_type=None):
         self.flat_attrs = flat_attrs
         self._type = resource_type
         self.source_json = source_json
 
     def is_ansible(self):
+        '''Check if the resource is provided by the ansible provider.'''
         return self.type().startswith("ansible_")
 
     def type(self):
+        '''Returns the Terraform resource type identifier.'''
         if self._type:
             return self._type
         return self.source_json["type"]
 
     def read_dict_attr(self, key):
+        '''
+        Read a dictionary attribute from the resource, handling old-style
+        Terraform state where maps are stored as multiple keys in the resource's
+        attributes.
+        '''
         attrs = self._raw_attributes()
 
         if self.flat_attrs:
@@ -100,6 +117,11 @@ class TerraformResource(object):
         return attrs.get(key, {})
 
     def read_list_attr(self, key):
+        '''
+        Read a list attribute from the resource, handling old-style Terraform
+        state where lists are stored as multiple keys in the resource's
+        attributes.
+        '''
         attrs = self._raw_attributes()
 
         if self.flat_attrs:
@@ -120,6 +142,9 @@ class TerraformResource(object):
         return attrs.get(key, None)
 
     def read_attr(self, key):
+        '''
+        Read an attribute from the underlaying state content.
+        '''
         return self._raw_attributes().get(key, None)
 
     def _raw_attributes(self):
@@ -129,12 +154,17 @@ class TerraformResource(object):
 
 
 class AnsibleInventory(object):
+    '''
+    AnsibleInventory handles conversion from Terraform resource content to
+    Ansible inventory entities, and building of the final inventory json.
+    '''
     def __init__(self):
         self.groups = {}
         self.hosts = {}
         self.inner_json = {}
 
     def update_hosts(self, hostname, groups=None, host_vars=None):
+        '''Upsert type action for host resources.'''
         if hostname in self.hosts:
             host = self.hosts[hostname]
             host.update(groups=groups, host_vars=host_vars)
@@ -147,6 +177,7 @@ class AnsibleInventory(object):
                 self.update_groups(groupname, hosts=[hostname])
 
     def update_groups(self, groupname, children=None, hosts=None, group_vars=None):
+        '''Upsert type action for group resources'''
         if groupname in self.groups:
             self.groups[groupname].update(
                 children=children, hosts=hosts, group_vars=group_vars)
@@ -155,6 +186,7 @@ class AnsibleInventory(object):
                 groupname, children=children, hosts=hosts, group_vars=group_vars)
 
     def add_host_resource(self, resource):
+        '''Convert from Terraform ansible_host resource.'''
         hostname = resource.read_attr("inventory_hostname")
         groups = resource.read_list_attr("groups")
         host_vars = resource.read_dict_attr("vars")
@@ -162,6 +194,7 @@ class AnsibleInventory(object):
         self.update_hosts(hostname, groups=groups, host_vars=host_vars)
 
     def add_host_var_resource(self, resource):
+        '''Convert from Terraform ansible_host_var resource.'''
         hostname = resource.read_attr("inventory_hostname")
         key = resource.read_attr("key")
         value = resource.read_attr("value")
@@ -171,6 +204,7 @@ class AnsibleInventory(object):
         self.update_hosts(hostname, host_vars=host_vars)
 
     def add_group_resource(self, resource):
+        '''Convert from Terraform ansible_group resource.'''
         groupname = resource.read_attr("inventory_group_name")
         children = resource.read_list_attr("children")
         group_vars = resource.read_dict_attr("vars")
@@ -178,6 +212,7 @@ class AnsibleInventory(object):
         self.update_groups(groupname, children=children, group_vars=group_vars)
 
     def add_group_var_resource(self, resource):
+        '''Convert from Terraform ansible_group_var resource.'''
         groupname = resource.read_attr("inventory_group_name")
         key = resource.read_attr("key")
         value = resource.read_attr("value")
@@ -187,6 +222,10 @@ class AnsibleInventory(object):
         self.update_groups(groupname, group_vars=group_vars)
 
     def add_resource(self, resource):
+        '''
+        Process a Terraform resource, passing to the correct handler function
+        by type.
+        '''
         if resource.type() == "ansible_host":
             self.add_host_resource(resource)
         elif resource.type() == "ansible_host_var":
@@ -197,6 +236,10 @@ class AnsibleInventory(object):
             self.add_group_var_resource(resource)
 
     def to_dict(self):
+        '''
+        Generate the file Ansible inventory structure to be serialized into JSON
+        for consumption by Ansible proper.
+        '''
         out = {
             "_meta": {
                 "hostvars": {}
@@ -215,6 +258,9 @@ class AnsibleInventory(object):
 
 
 class AnsibleHost(object):
+    '''
+    AnsibleHost represents a host for the Ansible inventory.
+    '''
     def __init__(self, hostname, groups=None, host_vars=None):
         self.hostname = hostname
         self.groups = set(["all"])
@@ -223,19 +269,25 @@ class AnsibleHost(object):
         self.update(groups=groups, host_vars=host_vars)
 
     def update(self, groups=None, host_vars=None):
+        '''Update host resource with additional groups and vars.'''
         if host_vars:
             self.host_vars.update(host_vars)
         if groups:
             self.groups.update(groups)
 
     def tidy(self):
+        '''Normalize host contents.'''
         self.groups = sorted(self.groups)
 
     def get_vars(self):
+        '''Get the host's variable dictionary.'''
         return dict(self.host_vars)
 
 
 class AnsibleGroup(object):
+    '''
+    AnsibleGroup represents a group for the Ansible inventory.
+    '''
     def __init__(self, groupname, children=None, hosts=None, group_vars=None):
         self.groupname = groupname
         self.hosts = set()
@@ -245,6 +297,9 @@ class AnsibleGroup(object):
         self.update(children=children, hosts=hosts, group_vars=group_vars)
 
     def update(self, children=None, hosts=None, group_vars=None):
+        '''
+        Update host resource with additional children, hosts, or group variables.
+        '''
         if hosts:
             self.hosts.update(hosts)
         if children:
@@ -253,10 +308,12 @@ class AnsibleGroup(object):
             self.group_vars.update(group_vars)
 
     def tidy(self):
+        '''Normailize group contents.'''
         self.hosts = sorted(self.hosts)
         self.children = sorted(self.children)
 
     def to_dict(self):
+        '''Prepare structure for final Ansible inventory JSON.'''
         return {
             "children": list(self.children),
             "hosts": list(self.hosts),
